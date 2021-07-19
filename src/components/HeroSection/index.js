82286ff6948a2ac5f5dfaadaf11cbf6a3504b24a
noerios <noerios@gmail.com>
2021-07-19 12:46:51 -0500

    video bg and hero elements styling

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Video from '../../videos/stockvideo.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
+import { Button } from '../ButtonElement'
+import { HeroContainer, 
+        HeroBg, 
+        VideoBg, 
+        HeroContent, 
+        HeroH1, 
+        HeroP, 
+        HeroBtnWrapper, 
+        ArrowForward, 
+        ArrowRight } from './HeroElements';
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>A San Antonio Law Firm, founded in '83</HeroH1>
+                <HeroP>
+                Dedicated to providing clients with professional, knowledgeable and attentive legal, counseling, and legislative services.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="contact" onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    primary="true"
+                    dark="true"
+                    >
+                        Get in touch {hover ? <ArrowForward /> : <ArrowRight />} 
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
